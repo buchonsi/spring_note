e878433307a398ed4af3f491fcd91f12c8b38d8c
buchonsi <buchonsi@naver.com>
2023-02-25 20:18:15 +0900

    [#8] 다른 Dao 추가
    Dao의 확장으로 factory에 중복 코드가 생성되었다.

--- src/main/java/me/learn/springnote/ver4/user/dao/DaoFactory.java
+++ src/main/java/me/learn/springnote/ver4/user/dao/DaoFactory.java
@@ -1,16 +1,18 @@
 package me.learn.springnote.ver4.user.dao;
 
 /**
- * 객체 생성방법을 결정하고 생성된 객체를 돌려주는 역할인 팩토리 생성.
- *         - 주의 : 디자인 패턴에서 말하는 추상 팩토리 패턴과 팩토리 메소드 패턴과는 다르다.
- *  남아있는 문제점 : 다른 기능을 하는 Dao가 추가 되었을때,
- *                 ConnectionMaker를 구현하는 클래스의 인스턴스 생성 코드가 중복되게 된다.
- *  해결 방법 : 오브젝트 팩토리의 활용
+ * Dao가 새로 만들졌을 때 코드의 중복발생
  */
 public class DaoFactory {
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(new DConnectionMaker());
+    }
+
+    public AccountDao accountDao() {
+        return new AccountDao(new DConnectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(new DConnectionMaker());
     }
 }
