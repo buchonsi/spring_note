a8af46e32d2aeec816b050e71fe1f715728ca57f
buchonsi <buchonsi@naver.com>
2023-02-25 19:32:13 +0900

    [#8] 팩토리 생성
    * ConnectionMaker의 구현객체를 선택하고 반환해주는 역할을 하는 팩토리 생성
    * 기능 분리를 하였으나 Dao 확장시 코드 중복 문제 발생

--- src/test/java/me/learn/springnote/ver4/user/dao/UserDaoTest.java
+++ src/test/java/me/learn/springnote/ver4/user/dao/UserDaoTest.java
@@ -8,13 +8,18 @@ import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * 문제점 : Test코드는 UserDao가 잘 동작하는지 테스트를 위해 만들어졌으나,
+ *        ConnectionMaker 구현 클래스를 선택하는 client 기능이 추가되었다.
+ *        성격이 다른 책임이나 관심사는 분리해야하므로 client 기능을 분리해야한다.
+ * 해결 방안 : 팩토리
+ */
 class UserDaoTest {
-    ConnectionMaker connectionMaker = new DConnectionMaker();
     @Test
     @DisplayName("유저 등록")
     void add_user() throws SQLException, ClassNotFoundException {
         //given
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         //when
         User user = new User();
@@ -33,7 +38,7 @@ class UserDaoTest {
     @DisplayName("유저 조회")
     void select_user() throws SQLException, ClassNotFoundException {
         //given
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("buchonsi");
